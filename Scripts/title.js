2cff0a6f78bf109fc9b120cd637f305bb3c5752d
Yaswanth-Tiruveedhula <yaswanth01sunny@gmail.com>
2025-06-19 23:29:52 +0530

    Add files via upload

---
+++ Scripts/title.js
@@ -0,0 +1,10 @@
+function startApp() {
+  window.location.href = "home.html";
+}
+function startApp1() {
+  window.location.href = "HTML/title.html";
+}
+
+function afterLogin(){
+  window.location.href = "features.html";
+}
